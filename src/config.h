ca7ba61a626f15c10c057b2594d671b526246392
Teimor Epstein <teimor.epstein@gmail.com>
2021-05-16 00:34:18 +0300

    update

--- src/config.h
+++ src/config.h
@@ -2,12 +2,10 @@
 
 #define HOSTNAME "roomba" // e.g. roomba.local
 #define BRC_PIN 14
-#define ROOMBA_650_SLEEP_FIX 1
+#define ROOMBA_650_SLEEP_FIX 0
 
 #define ADC_VOLTAGE_DIVIDER 44.551316985
 //#define ENABLE_ADC_SLEEP
 
-#define MQTT_SERVER "10.0.0.2"
-#define MQTT_USER "homeassistant"
 #define MQTT_COMMAND_TOPIC "vacuum/command"
 #define MQTT_STATE_TOPIC "vacuum/state"
--- src/secrets.example.h
+++ src/secrets.example.h
@@ -1,3 +1,6 @@
+#define MQTT_SERVER "10.0.0.2"
+#define MQTT_PORT 1883
+#define MQTT_USER "homeassistant"
 #define MQTT_PASSWORD "mysecurepassword"
-#define WIFI_PASSWORD "password1234"
 #define WIFI_SSID "Home Wifi"
+#define WIFI_PASSWORD "password1234"
