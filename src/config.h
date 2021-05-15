934cfba1e0d342a8fe64d86f45f2431a51e3df1e
Teimor Epstein <teimor.epstein@gmail.com>
2021-05-16 02:19:41 +0300

    Implement MQTT discovery support

--- src/config.h
+++ src/config.h
@@ -7,5 +7,12 @@
 #define ADC_VOLTAGE_DIVIDER 44.551316985
 //#define ENABLE_ADC_SLEEP
 
-#define MQTT_COMMAND_TOPIC "vacuum/command"
-#define MQTT_STATE_TOPIC "vacuum/state"
+// Only change if you know what you're doing!
+#define MQTT_DISCOVERY "homeassistant"
+#define MQTT_DEVICE_CLASS "vacuum"
+#define MQTT_DIVIDER "/"
+#define MQTT_TOPIC_BASE MQTT_DISCOVERY MQTT_DIVIDER MQTT_DEVICE_CLASS MQTT_DIVIDER
+#define MQTT_IDPREFIX "roomba_"
+#define MQTT_COMMAND_TOPIC "command"
+#define MQTT_STATE_TOPIC "state"
+#define MQTT_CONFIG_TOPIC "config"
